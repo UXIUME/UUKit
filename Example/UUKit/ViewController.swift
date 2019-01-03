47a78713eafac3261d6bae35054b59e22e90e937
😸 <mail2me@sina.cn>
2019-01-03 14:09:28 +0800

    no message

---
+++ Example/UUKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UUKit
+//
+//  Created by mail@uxiu.me on 01/03/2019.
+//  Copyright (c) 2019 mail@uxiu.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
